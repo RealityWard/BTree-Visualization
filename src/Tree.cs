d04d18e5cdc57e04f583d7d9d7b0e42ae3dcd033
RealityWard <tristanlanderson@gmail.com>
2024-02-12 17:10:42 -0700

    Fixed up forgotten bits about handling splits at root. This triggered looking into converting nullable vars and resorted to changing the root to start with a leaf node by default. The other nullables have a conditional replacement with new objects. These new objects will never be put in though due to the check for nulls ahead of time. Thus the conditional replacement on false will never be reached. I only put those in because visual code will not trust that I'm checking ahead of time.

--- src/Tree.cs
+++ src/Tree.cs
@@ -3,43 +3,70 @@ Author: Tristan Anderson
 Date: 2024-02-03
 Desc: Maintains the entry point of the BTree data structure and initializes root and new node creation in the beginning.
 */
+using System.ComponentModel;
 using NodeData;
 
 namespace BTreeVisualization{
   public class BTree{
-    private BTreeNode? _Root;
+    private BTreeNode _Root;
     private int _Degree;
     public BTree(int degree){
-      _Root = null;
+      _Root = new LeafNode(degree);
       _Degree = degree;
-      Console.WriteLine(_Degree);
     }
     
+    /// <summary>
+    /// Takes a node and the key and data to place into root.
+    /// </summary>
+    /// <param name="rNode"></param>
+    private void Split(((int,Data),Node) rNode){
+      _Root = new NonLeafNode(_Degree,[rNode.Item1.Item1],[rNode.Item1.Item2],[_Root, rNode.Item2]);
+    }
+    
+    /// <summary>
+    /// Inserts data at root node and set root to a new leaf node if root isn't yet created. It also checks for a split afterwards.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="data"></param>
     public void Insert(int key, Data data){
       if(_Root == null){
-        // _Root = new LeafNode(_Degree);
-        // _Root.InsertKey(key, data);
+        _Root = new LeafNode(_Degree);
+        _Root.InsertKey(key, data);
       }else{
-        if(_Root.GetNumKeys() == 2*_Degree-1){
-          // BTreeNode node = new NonLeafNode(_Degree);
-          // node.InsertChild(key, _Root);
-          // _Root = node;
-        }else{
-          _Root.SearchKey(key).Item2.InsertKey(key, data);
+        ((int,Data?),Node?) result = _Root.InsertKey(key, data);
+        if(result.Item2 != null && result.Item1.Item2 != null){
+          Split(((result.Item1.Item1,true? result.Item1.Item2 : new Person("Placeholder")),true? result.Item2 : new LeafNode(_Degree)));
         }
       }
     }
 
+    /// <summary>
+    /// Using searchKey on the nodes to find the entry and calls delete on the node returned. If index is -1 it does nothing.
+    /// </summary>
+    /// <param name="key"></param>
     public void Delete(int key){
-      _Root?.SearchKey(key).Item2.DeleteKey(key);
+      (int,Node) result = _Root.SearchKey(key);
+      if(result.Item1 != -1){
+        result.Item2.DeleteKey(key);
+      }
     }
 
-    public (int,Node?) Search(int key){
-      return _Root != null ? _Root.SearchKey(key) : (-1,null);
+    /// <summary>
+    /// Using searchKey on the nodes to return the data correlated to the key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public Data? Search(int key){
+      (int,Node?) result = _Root.SearchKey(key);
+      if (result.Item1 == -1 || result.Item2 == null){
+        return null;
+      }else{
+        return ((BTreeNode)result.Item2).Contents[result.Item1];
+      }
     }
 
     public string Traverse(){
-      return _Root != null ? _Root.Traverse() : "";
+      return _Root.Traverse();
     }
   }
 }
