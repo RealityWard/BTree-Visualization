9da40acf405f0a7b98dbf3584fb9e2f1cf52e7eb
Andreas Kramer <andreaskramer@isu.edu>
2024-03-27 19:13:23 -0600

    tests

--- tests/BPlusInsertionUnitTest.cs
+++ tests/BPlusInsertionUnitTest.cs
@@ -7,9 +7,14 @@ namespace BPlustests{
   [TestFixture]
   public partial class BPlusInsertionUnitTests{
     private BPlusTree<Person> _Tree;
+    private BPlusTree<Person> _Tree2;
+    private BPlusTree<Person> _Tree3;
+
     [SetUp]
     public void Setup(){
         _Tree = new(3,new BufferBlock<(NodeStatus status, long id, int numKeys, int[] keys, Person?[] contents, long altID, int altNumKeys, int[] altKeys, Person?[] altContents)>());
+        _Tree2 = new(5,new BufferBlock<(NodeStatus status, long id, int numKeys, int[] keys, Person?[] contents, long altID, int altNumKeys, int[] altKeys, Person?[] altContents)>());
+        _Tree3 = new(6,new BufferBlock<(NodeStatus status, long id, int numKeys, int[] keys, Person?[] contents, long altID, int altNumKeys, int[] altKeys, Person?[] altContents)>());
     }
   
     /// <summary>
@@ -27,6 +32,8 @@ namespace BPlustests{
         string name = $"Person {i}";
         if (!insertedKeys.ContainsKey(key)){
           _Tree.Insert(key, new Person(name));
+          _Tree2.Insert(key, new Person(name));
+          _Tree3.Insert(key,new Person(name));
           insertedKeys.Add(key, name);
         }
         else{
@@ -38,10 +45,38 @@ namespace BPlustests{
         int key = entry.Key;
         string expectedName = entry.Value;
         var result = _Tree.Search(key);
+        var result2 = _Tree.Search(key);
+        var result3 = _Tree.Search(key);
             
         Assert.That(result, Is.Not.Null, $"Key {key} should be found.");
         Assert.That(result.Name, Is.EqualTo(expectedName), $"Key {key} should return the correct person.");
+        Assert.That(result2, Is.Not.Null, $"Key {key} should be found.");
+        Assert.That(result2.Name, Is.EqualTo(expectedName), $"Key {key} should return the correct person.");
+        Assert.That(result3, Is.Not.Null, $"Key {key} should be found.");
+        Assert.That(result3.Name, Is.EqualTo(expectedName), $"Key {key} should return the correct person.");
+
       }
     }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(4)]
+    [TestCase(10)]
+    [TestCase(100)]
+    public void InsertionBaseTest(int x){
+      for(int i = 0; i < x; i++){
+          _Tree.Insert(i,new Person(i.ToString()));
+      }
+      for(int i = 0; i < x; i++){
+          Assert.That(_Tree.Search(i), Is.Not.Null, "Search for key " + i + " was not found.");
+          if (_Tree.Search(i) != null){
+            #pragma warning disable CS8602 // Dereference of a possibly null reference.
+            Assert.That(_Tree.Search(i).Name, Is.EqualTo(i.ToString()), "Search turns up wrong content.");
+            #pragma warning restore CS8602 // Dereference of a possibly null reference.
+          }
+        }
+        Assert.That(Regex.Count(_Tree.Traverse(),"name"), Is.EqualTo(x), "Missing insertions");
+        }
   }
 }
