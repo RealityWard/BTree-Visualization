2fa3ea01f23b2b5aa0f68af957288b461e7d6a65
RealityWard <tristanlanderson@gmail.com>
2024-04-28 21:03:20 -0600

    Fixed more warnings

--- src/gui/PopupForm.cs
+++ src/gui/PopupForm.cs
@@ -1,28 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using ThreadCommunication;
-
-namespace B_TreeVisualizationGUI
+﻿namespace B_TreeVisualizationGUI
 {
     public partial class PopupForm : Form
     {
-        public PopupForm()
-        {
-            InitializeComponent();
-        }
-
-        private Form1 mainForm = null;
+        private Form1 mainForm;
         public PopupForm(Form callingForm)
         {
-            mainForm = callingForm as Form1;
+            mainForm = (Form1)callingForm;
             InitializeComponent();
         }
 
@@ -55,19 +38,19 @@ namespace B_TreeVisualizationGUI
 
         private void btnSearchRange_Click(object sender, EventArgs e)
         {
-            this.mainForm.SearchOrDeleteOverRange(txtStart.Text, txtEnd.Text, "search");
-            this.Close();
+            mainForm.SearchOrDeleteOverRange(txtStart.Text, txtEnd.Text, "search");
+            Close();
         }
 
         private void btnDeleteRange_Click(object sender, EventArgs e)
         {
-            this.mainForm.SearchOrDeleteOverRange(txtStart.Text, txtEnd.Text, "delete");
-            this.Close();
+            mainForm.SearchOrDeleteOverRange(txtStart.Text, txtEnd.Text, "delete");
+            Close();
         }
 
         private void PopupForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            this.mainForm.EnableButtonEvents();
+            mainForm.EnableButtonEvents();
         }
     }
 }
